00855cbd6d581e17ca3e943ef1ddd50128dc9722
Yong Du <y.du@live.com>
2019-10-31 17:51:48 -0400

    measure expression

--- TOMWrapperTest/ModelMetadataTests.cs
+++ TOMWrapperTest/ModelMetadataTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TabularEditor.TOMWrapper;
 
@@ -12,11 +13,36 @@ namespace TOMWrapperTest
         public TestContext TestContext { get; set; }
 
         [TestMethod]
+        public void DataSourceTest()
+        {
+            var orgModel = new TabularModelHandler("AdventureWorks2.bim");
+            var datasources = orgModel.Model.DataSources;
+            TestContext.WriteLine("Data Sources:");
+            foreach(var ds in datasources)
+            {
+                var pds = ds as ProviderDataSource;
+                if (pds != null)
+                {
+                    TestContext.WriteLine($"name: {pds.Name}");
+                    TestContext.WriteLine($"connectionString: {pds.ConnectionString}");
+                    TestContext.WriteLine($"provider: {pds.Provider}");
+                }
+            }
+        }
+        [TestMethod]
         public void TableListTest()
         {
             var orgModel = new TabularModelHandler("AdventureWorks2.bim");
             var tables = orgModel.Model.Tables;
             Assert.AreEqual(14, tables.Count);
+            TestContext.WriteLine("Tables having measures:");
+            foreach(var t in tables)
+            {
+                if(t.Measures?.Count > 0)
+                {
+                    TestContext.WriteLine($"Table [{t.Name}] has {t.Measures.Count} measures");
+                }
+            }
         }
 
         [TestMethod]
@@ -25,6 +51,49 @@ namespace TOMWrapperTest
             var orgModel = new TabularModelHandler("AdventureWorks2.bim");
             var measures = orgModel.Model.AllMeasures.ToList();
             Assert.AreEqual(58, measures.Count);
+        }
+
+        [TestMethod]
+        public void GetMeasureByNameTest()
+        {
+            var orgModel = new TabularModelHandler("AdventureWorks2.bim");
+            var measures = orgModel.Model.AllMeasures.ToList();
+            Assert.AreEqual(58, measures.Count);
+            string measureName = "Distinct Count Sales Orders";
+            var measure = measures.Where(m => m.Name == measureName).FirstOrDefault();
+            Assert.IsNotNull(measure);
+            Assert.AreEqual(measureName, measure.Name);
+            Assert.AreEqual("[Reseller Distinct Count Sales Order] + [Internet Distinct Count Sales Order]", measure.Expression);
+            Assert.AreEqual("Sales Territory", measure.Table.Name);
+        }
+
+        [TestMethod]
+        public void GetRelatedTablesInMeasureTest()
+        {
+            var orgModel = new TabularModelHandler("AdventureWorks2.bim");
+            var measures = orgModel.Model.AllMeasures.ToList();
+            Assert.AreEqual(58, measures.Count);
+            string measureName = "Distinct Count Sales Orders";
+            var measure = measures.Where(m => m.Name == measureName).FirstOrDefault();
+            Assert.IsNotNull(measure);
+            Assert.AreEqual(measureName, measure.Name);
+            Assert.AreEqual("[Reseller Distinct Count Sales Order] + [Internet Distinct Count Sales Order]", measure.Expression);
+            Assert.AreEqual("Sales Territory", measure.Table.Name);
+
+            // process expression to leaf level measure
+            foreach (var m in measures)
+            {
+                //var regx = new Regex(@"(?<func1>[a-z]+)\(\[(?<numerator>.+)\]\)/(?<func2>[a-z]+)\(\[(?<denominator>.+)\]\)", RegexOptions.IgnoreCase);
+                var regx = new Regex(@"(?<=\[).+?(?=\])", RegexOptions.IgnoreCase);
+                var match = regx.Match(m.Expression);
+                if (match.Success)
+                {
+                    var func1 = match.Groups["func1"].Value;
+                    var func2 = match.Groups["func2"].Value;
+                    var numerator = match.Groups["numerator"];
+                    var denominator = match.Groups["denominator"];
+                }
+            }
         }
 
         [TestMethod]
