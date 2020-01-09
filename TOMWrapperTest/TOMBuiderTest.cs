66a21c1661ceca4c8a6553864d8984375de3561d
Yong Du <y.du@live.com>
2020-01-09 17:04:16 -0500

    test updated to create a simple model

--- TOMWrapperTest/TOMBuiderTest.cs
+++ TOMWrapperTest/TOMBuiderTest.cs
@@ -21,10 +21,47 @@ namespace AibiSoft.Data.Tests
 
             var databases = dbConnectin.Databases;
             var db = databases["DSS2"];
-            var table = db.Tables.Where(t=>t.Name == "Test_Vint").First();
+            
             var builder = new TOMBuilder();
-            builder.AddTable(table);
+            // get model
+            var model = builder.Model;
 
+            // datasource
+            var ds = model.AddDataSource("My data source name");
+            ds.ConnectionString = connStr;
+            ds.Provider = "System.Data.SqlClient";
+            ds.Password = "xyz123";
+            ds.ImpersonationMode = TabularEditor.TOMWrapper.ImpersonationMode.Default;
+            // add tables
+            builder.AddTable(db.Tables.Where(t => t.Name == "Test_Vint").First());
+            builder.AddTable(db.Tables.Where(t => t.Name == "Test_Orig").First());
+            builder.AddTable(db.Tables.Where(t => t.Name == "Test_GroupControl").First());
+            builder.AddTable(db.Tables.Where(t => t.Name == "Test_Date").First());
+            
+            // add relationships
+
+            // vint.origination_id => orig.id
+            var vint_fk_origination_id = model.Tables["Test_Vint"].Columns["Origination_ID"];
+            var orig_pk_id = model.Tables["Test_Orig"].Columns["ID"];
+            vint_fk_origination_id.RelateTo(orig_pk_id);
+            // orig.groupcontrol_id => groupcontrol.id
+            var orig_fk_groupcontrol_id = model.Tables["Test_Orig"].Columns["GroupControl_ID"];
+            var groupcontrol_pk_id = model.Tables["Test_GroupControl"].Columns["ID"];
+            orig_fk_groupcontrol_id.RelateTo(groupcontrol_pk_id);
+            // vint.month => date.DateKey
+            var vint_fk_month = model.Tables["Test_Vint"].Columns["Month"];
+            var date_datekey = model.Tables["Test_Date"].Columns["DateKey"];
+            vint_fk_month.RelateTo(date_datekey);
+            // orig.vintage_date => date.DateKey
+            /*
+            var vint_fk_vintage_date = model.Tables["Test_Orig"].Columns["Vintage_Date"];
+            vint_fk_month.RelateTo(date_datekey);
+            */
+            var rel = model.AddRelationship();
+            rel.FromColumn = vint_fk_month;
+            rel.ToColumn = date_datekey;
+
+            builder.Save("dsstest.json");
             //var model = new StarModel("select * from [Alphabetical list of products]");
             //model.Init(dbConnection);
         }
