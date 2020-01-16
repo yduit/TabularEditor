4c8b6b0a5ba5f8e4c33fb2b6caf39ff253318654
Yong Du <y.du@live.com>
2020-01-16 14:34:47 -0500

    remove old method

--- TOMWrapperTest/TOMBuiderTest.cs
+++ TOMWrapperTest/TOMBuiderTest.cs
@@ -15,63 +15,6 @@ namespace AibiSoft.Data.Tests
     public class TOMBuilderTest
     {
         [TestMethod]
-        public void SchemaTableTest1()
-        {
-            string connStr = "data source=localhost;initial catalog=DSS2;Integrated Security=sspi";
-            var dbConnectin = new DatabaseConnectionManager(connStr, ADatabaseMetadataDiscovery.SQLServer);
-
-            var databases = dbConnectin.Databases;
-            var db = databases["DSS2"];
-            
-            var builder = new TOMBuilder();
-            // get model
-            var model = builder.Model;
-
-            // datasource
-            var ds = model.AddDataSource("My data source name");
-            ds.ConnectionString = connStr;
-            ds.Provider = "System.Data.SqlClient";
-            ds.Password = "xyz123";
-            ds.ImpersonationMode = TabularEditor.TOMWrapper.ImpersonationMode.ImpersonateAccount;
-            // add tables
-            builder.AddTable0(db.Tables.Where(t => t.Name == "Test_Vint").First());
-            builder.AddTable0(db.Tables.Where(t => t.Name == "Test_Orig").First());
-            builder.AddTable0(db.Tables.Where(t => t.Name == "Test_GroupControl").First());
-            builder.AddTable0(db.Tables.Where(t => t.Name == "Test_Date").First());
-            
-            // add relationships
-
-            // vint.origination_id => orig.id
-            var vint_fk_origination_id = model.Tables["Test_Vint"].Columns["Origination_ID"];
-            var orig_pk_id = model.Tables["Test_Orig"].Columns["ID"];
-            vint_fk_origination_id.RelateTo(orig_pk_id);
-            // orig.groupcontrol_id => groupcontrol.id
-            var orig_fk_groupcontrol_id = model.Tables["Test_Orig"].Columns["GroupControl_ID"];
-            var groupcontrol_pk_id = model.Tables["Test_GroupControl"].Columns["ID"];
-            orig_fk_groupcontrol_id.RelateTo(groupcontrol_pk_id);
-            // vint.month => date.DateKey
-            var vint_fk_month = model.Tables["Test_Vint"].Columns["Month"];
-            var date_datekey = model.Tables["Test_Date"].Columns["DateKey"];
-            vint_fk_month.RelateTo(date_datekey);
-            // orig.vintage_date => date.DateKey
-            var vint_fk_vintage_date = model.Tables["Test_Orig"].Columns["Vintage_Date"];
-            /*
-            
-            vint_fk_month.RelateTo(date_datekey);
-            */
-            var rel = model.AddRelationship();
-            rel.FromColumn = vint_fk_vintage_date;
-            rel.ToColumn = date_datekey;
-
-            // add sum over sum measures
-            model.Tables["Test_Vint"].AddMeasure("PD2", "[Sum_of_Default_Accounts]/[Sum_of_Booked_Accounts]");
-            model.Tables["Test_Vint"].AddMeasure("AD2", "Sum('Test_Vint'[Active_Accounts])/Sum('Test_Vint'[Default_Accounts])");
-
-            builder.Save("dsstest.json");
-
-        }
-
-        [TestMethod]
         public void RelationshipTest()
         {
             var builder = new TOMBuilder("dsstest.json");
